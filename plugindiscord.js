89836720b3c55102af8dc44db01da2034d3b1aa3
YouCannotHide <liam021002@gmail.com>
2017-03-29 19:30:23 +0100

    Add files via upload

---
+++ plugindiscord.js
@@ -0,0 +1,56 @@
+const ytdl    = require('youtube-dl')
+const fs      = require('fs')
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const bot = new PlugAPI({ guest: true })
+var PlugAPI = require('plugapi');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+var message = 'msg'
+var prefix = "!"
+
+client.on('ready',() => {
+  client.user.setGame("on plug.dj");
+	console.log('I\'m Online\nI\'m Online');
+});
+
+bot.connect('YOUR ROOM!'); // The part after https://plug.dj
+
+bot.on('roomJoin', function(room) {
+    console.log("Joined " + room);
+});
+
+
+/* ----- FUNCTIONS N STUFF ----- */
+
+function getMedia() {
+    const media = bot.getMedia()
+    const obj = {
+        title: `${media.author} - ${media.title}`,
+        cid: media.cid,
+        duration: media.duration,
+        format: media.format,
+        elapsed: bot.getTimeElapsed(),
+    }
+    if (obj.format === 1) {
+        obj.url = `https://www.youtube.com/watch?v=${media.cid}`
+    } else {
+        obj.url = `https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/${media.cid}`
+    }
+    return obj
+}
+
+/* ----- COMMANDS ----- */
+
+
+client.on('message', message => {
+	if (message.content.startsWith(prefix + 'nowplaying')) {
+    message.channel.sendMessage('**Current Song Playing:** ' + getMedia().title )
+	}
+});
+
+
+client.login('Mjk2MzA3MjkyMTIxNTk1OTA0.C7wVLg.ze5lK3r7-sqQ-CIn2eEeRbfa5Gw');
